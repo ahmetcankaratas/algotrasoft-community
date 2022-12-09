c1390d97c8784ed1a081d33e2907c70d4939dfaf
ahmetcankaratas <karatascanahmet@yahoo.com>
2022-12-09 16:20:37 +0300

    add header,

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import React, { Fragment, useState } from "react";
+import Header from "./components/Layout/Header";
+
 function App(): React.ReactElement {
-  return <div className="text-red-500">Hello World !</div>;
+
+  return (
+    <div
+      className={`mx-auto h-full w-full py-2 font-opensans dark:bg-darkBlue dark:text-white`}
+    >
+      <Header />
+    </div>
+  );
 }
 
 export default App;
